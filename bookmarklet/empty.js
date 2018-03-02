0131be71f208aaa009a3b49814d309ec17ca157f
Matthias Pitscher <schaefer9111@gmail.com>
2018-03-03 00:18:11 +0100

    init

---
+++ bookmarklet/empty.js
@@ -0,0 +1,50 @@
+(function () {
+  let ps = document.getElementsByTagName('p');
+  for (let i = 0; i < ps.length; i++) {
+    ps[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h1s = document.getElementsByTagName('h1');
+  for (let i = 0; i < h1s.length; i++) {
+    h1s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h2s = document.getElementsByTagName('h2');
+  for (let i = 0; i < h2s.length; i++) {
+    h2s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h3s = document.getElementsByTagName('h3');
+  for (let i = 0; i < h3s.length; i++) {
+    h3s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h4s = document.getElementsByTagName('h4');
+  for (let i = 0; i < h4s.length; i++) {
+    h4s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h5s = document.getElementsByTagName('h5');
+  for (let i = 0; i < h5s.length; i++) {
+    h5s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h6s = document.getElementsByTagName('h6');
+  for (let i = 0; i < h6s.length; i++) {
+    h6s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let h7s = document.getElementsByTagName('h7');
+  for (let i = 0; i < h7s.length; i++) {
+    h7s[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let as = document.getElementsByTagName('a');
+  for (let i = 0; i < as.length; i++) {
+    as[i].style['color'] = 'rgba(0,0,0,0)';
+  }
+  let imgs = document.getElementsByTagName('img');
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].style['opacity'] = '0';
+  }
+  let vids = document.getElementsByTagName('video');
+  for (let i = 0; i < vids.length; i++) {
+    vids[i].style['opacity'] = '0';
+  }
+  let svgs = document.getElementsByTagName('svg');
+  for (let i = 0; i < svgs.length; i++) {
+    svgs[i].style['opacity'] = '0';
+  }
+})();
